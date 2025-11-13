84a2d6625cefbd8dc8011a7f0f01097af8aed589
amanhappysingh <amans.happysingh@gmail.com>
2025-11-14 00:18:16 +0530

    add

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,7 @@
 export const URL = {
 // baseUrl :   'http://192.168.1.5:9080',
-baseUrl :   'http://127.0.0.1:9080',
+// baseUrl :   'http://127.0.0.1:9080',
+baseUrl :   'https://ews-api.amperevision.in',
 login : '/v1/auth/login',
 role : '/v1/users/roles',
 users : '/v1/users',
